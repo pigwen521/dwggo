430bfc64c78d9e64da6873411bc9fb361d38cecc
lishaowen <lishaowen@daishujiankang.com>
2022-09-01 09:46:20 +0800

    使用进程查询方式代替pid文件方式重写restart

--- go.mod
+++ go.mod
@@ -8,7 +8,9 @@ require (
 	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/gorilla/sessions v1.2.1
 	github.com/natefinch/lumberjack v2.0.0+incompatible
+	github.com/shirou/gopsutil v3.21.11+incompatible
 	github.com/spf13/viper v1.12.0
+	github.com/tklauser/go-sysconf v0.3.10 // indirect
 	go.uber.org/zap v1.21.0
 	golang.org/x/time v0.0.0-20220609170525-579cf78fd858
 	gorm.io/driver/mysql v1.3.4
--- system/mygin/gin.go
+++ system/mygin/gin.go
@@ -3,59 +3,48 @@ package mygin
 import (
 	"context"
 	"errors"
-	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
-	"os/exec"
 	"os/signal"
-	"strconv"
 	"syscall"
 	"time"
 
 	"dsjk.com/dwggo/system/core"
-	"dsjk.com/dwggo/system/lib/helper"
+	"dsjk.com/dwggo/system/lib/helper/arrmap"
 	"dsjk.com/dwggo/system/lib/helper/str"
-	"dsjk.com/dwggo/system/lib/helper/str/verify"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/render"
+	"github.com/shirou/gopsutil/net"
+	"github.com/shirou/gopsutil/process"
 	"golang.org/x/time/rate"
 )
 
-var pid_file string       //save PID for stop
 var limiter *rate.Limiter //限流器
 var MyginEngin *gin.Engine
 
-func init() {
-	port := core.GetConfigString("app.port")
-	pid_file = core.GetConfigString("logger.custom_dir") + "gin_" + port + ".pid"
-}
-
-func Stop() {
-	old_pid, err := getOldPid()
+func Stop() *process.Process {
+	old_pid, old_proc, err := getOldPidProc()
 	if err != nil {
-		cmdLog("stop error: pid read error " + err.Error() + ",pid file:" + pid_file)
-		return
+		cmdLog("stop error: pid found error " + err.Error())
+		return nil
 	}
-	if old_pid == "" {
-		cmdLog("stop error: pid is not found")
-		return
+	if old_pid == 0 {
+		cmdLog("stop skip: old pid is not found")
+		return nil
 	}
-	if !verify.IsNumber(&old_pid) { //防止pid文件被注入
-		cmdLog("stop error: pid is not number")
-		return
-	}
-	out, err := exec.Command("sh", "-c", "kill "+old_pid).Output()
+	err = old_proc.Terminate()
 	if err != nil {
-		cmdLog("stop error: cmd exec error,pid:" + old_pid + ",err:" + err.Error())
+		cmdLog("stop error: kill error,pid:" + str.ToString(old_pid) + ",err:" + err.Error())
+		return nil
 	} else {
-		cmdLog("stop success:" + string(out))
+		//cmdLog("stop signal send success")
+		return old_proc
 	}
 }
 
 func ReStart(gin *gin.Engine, initCtrlByNameCB InitCtrlByNameCB) {
-	Stop()
-	err := waitStop(3)
+	old_proc := Stop()
+	err := waitStop(old_proc, 3)
 	if err != nil {
 		cmdLog("waitStop error:" + err.Error())
 		cmdLog("try start...")
@@ -64,23 +53,34 @@ func ReStart(gin *gin.Engine, initCtrlByNameCB InitCtrlByNameCB) {
 }
 
 //等待先前启动的进程停止了再start
-func waitStop(waitSecond int) error {
-	old_pid, err := getOldPid()
-	if err != nil {
-		return errors.New("pid read error " + err.Error() + ",pid file:" + pid_file)
+func waitStop(old_proc *process.Process, waitSecond int) error {
+	if old_proc == nil {
+		return nil
 	}
-	if old_pid == "" {
-		return nil //等待好了
+	_, old_proc, err := getProcessByPid(old_proc.Pid)
+	if err != nil {
+		return err
+	}
+	if old_proc == nil { //进程消失了
+		return nil
+	}
+
+	old_status, err := old_proc.Status()
+	if err != nil {
+		return errors.New("old process status error " + err.Error())
+	}
+	if !arrmap.InArrayStr(old_status, []string{"R", "S", "D"}) { //R运行，S中断，D不可中断，Z僵死，T停止
+		return nil //算等待好了
 	}
 	if waitSecond == 0 { //等待超时了
-		return errors.New("timeout")
+		return errors.New("stop timeout")
 	}
-	cmdLog("waitStop ...")
-	if old_pid != "" { //继续等待
-		time.Sleep(time.Second)
-		return waitStop(waitSecond - 1)
-	}
-	return nil
+	//继续等待
+	//if arrmap.InArrayStr(old_status, []string{"R", "S", "D"}) {
+	cmdLog("waitStop ... process status: " + old_status)
+	time.Sleep(time.Second)
+	return waitStop(old_proc, waitSecond-1)
+	//}
 }
 
 func Start(gin *gin.Engine, initCtrlByNameCB InitCtrlByNameCB) {
@@ -99,10 +99,7 @@ func Start(gin *gin.Engine, initCtrlByNameCB InitCtrlByNameCB) {
 	forShutdown(srv)              //优雅关闭
 }
 func listenAndServer(srv *http.Server, port string) {
-	pid := getPid()
-	cmdLog("Listening and serving HTTP on " + srv.Addr + ",pid:" + pid)
-	old_pid, _ := getOldPid()
-	helper.WriteFile(pid_file, &pid)
+	pid := getNowPid()
 
 	var err error
 	if core.GetConfigString("app.scheme") == "https" { //HTTPS证书
@@ -111,49 +108,124 @@ func listenAndServer(srv *http.Server, port string) {
 		if certFile == "" || keyFile == "keyFile" {
 			core.LogErrorAndPanic("https时certFile,keyFile不能为空")
 		}
+		cmdLog("Listening and serving HTTPS on " + srv.Addr + ",pid:" + str.ToString(pid))
 		err = srv.ListenAndServeTLS(certFile, keyFile)
 	} else {
+		cmdLog("Listening and serving HTTP on " + srv.Addr + ",pid:" + str.ToString(pid))
 		err = srv.ListenAndServe()
 	}
 
 	if err != nil && err != http.ErrServerClosed {
 		cmdLog("start error:" + err.Error())
-		helper.WriteFile(pid_file, &old_pid) //启动失败，写回去
 		os.Exit(0)
 	}
-	cmdLog("ListenAndServe end,pid:" + pid)
+	cmdLog("old server listenAndServe end,pid:" + str.ToString(pid))
 }
 func cmdLog(str string) {
 	core.LogInfoCustomFmt(str, "cmd")
 }
-func getPid() string {
-	return strconv.Itoa(os.Getpid())
-}
-func getOldPid() (string, error) {
-	pid, err := ioutil.ReadFile(pid_file)
-	if err != nil {
-		fmt.Println(err)
-		return "", err
+
+var now_pid int32 //当前启动程序的PID
+//var old_pid int32 //先前启动程序的PID
+
+func getNowPid() int32 {
+	if now_pid != 0 {
+		return now_pid
 	}
-	return string(pid), err
+	now_pid = (int32)(os.Getpid())
+	return now_pid
+}
+
+func getOldPidProc() (int32, *process.Process, error) {
+	//先获取当前进程
+	now_proc_name, _, err := getProcessByPid(getNowPid())
+	if err != nil {
+		return 0, nil, err
+	}
+
+	//再根据端口号获取先前进程
+	old_pid, err := getPidByPort(core.GetConfigInt("app.port"))
+	if err != nil {
+		return 0, nil, err
+	}
+	if old_pid == 0 { //先前还没启动
+		return 0, nil, nil
+	}
+
+	old_proc_name, old_proc, err := getProcessByPid(old_pid)
+	if err != nil {
+		return 0, nil, err
+	}
+	if old_proc == nil { //不存在
+		return 0, nil, nil
+	}
+
+	//当前和先前进程的进程名一致，才确保PID正确，才能被kill
+	if old_proc_name == now_proc_name {
+		return old_pid, old_proc, nil
+	} else {
+		cmdLog("get the old_pid error,old_proc_name!=now_proc_name," + old_proc_name + "!=" + now_proc_name)
+		return 0, nil, nil
+	}
+}
+
+func getPidByPort(port_in int) (int32, error) {
+	port := (uint32)(port_in)
+	net_conns, err := net.Connections("tcp")
+	//{"fd":130,"family":2,"type":1,"localaddr":{"ip":"10.0.3.163","port":63503},"remoteaddr":{"ip":"110.43.121.209","port":443},"status":"CLOSE_WAIT","uids":null,"pid":65919}
+	if err != nil {
+		return 0, err
+	}
+	var find_net net.ConnectionStat
+	for _, cs := range net_conns {
+		if cs.Laddr.Port == port {
+			find_net = cs
+		}
+	}
+	if find_net.Pid == 0 {
+		return 0, nil
+	}
+	return find_net.Pid, nil
+}
+func getProcessByName(name string) *process.Process {
+	pids, _ := process.Pids()
+	for _, pid := range pids {
+		pn, _ := process.NewProcess(pid)
+		pn_name, _ := pn.Name()
+		if pn_name == name {
+			return pn
+		}
+	}
+	return nil
+}
+func getProcessByPid(pid int32) (string, *process.Process, error) {
+	now_pn, err := process.NewProcess(pid)
+	if errors.Is(err, process.ErrorProcessNotRunning) {
+		return "", nil, nil
+	}
+	if err != nil {
+		return "", nil, err
+	}
+	now_pName, err := now_pn.Name()
+	return now_pName, now_pn, err
 }
 
 //graceful  优雅关闭
 func forShutdown(srv *http.Server) {
 	quit := make(chan os.Signal)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM) //ctrl+c=>os.Interrt  kill -9=>os.Kill ,kill xx =>syscall.SIGTERM
+	signal.Notify(quit, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM) //退出终端=》syscall.SIGHUP；ctrl+c=>os.Interrt；kill xx =>syscall.SIGTERM,Terminate;；kill -9=>os.Kill syscall.SIGKILL 忽略 无法被接收
 	got := <-quit
 
-	cmdLog("server shutdown start:" + got.String())
+	cmdLog("old server shutdown start:" + got.String())
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
-		cmdLog("Server Shutdown error:" + err.Error())
+		cmdLog("old Server shutdown error :" + err.Error())
 	} else {
-		cmdLog("server shutdown success,oldpid:" + getPid())
+		cmdLog("old server shutdown success ^_^,oldpid:" + str.ToString(getNowPid()))
 	}
-	empty_pid := ""
-	helper.WriteFile(pid_file, &empty_pid)
+	/* empty_pid := ""
+	helper.WriteFile(pid_file, &empty_pid) */
 }
 
 func ginInit(r *gin.Engine, initCtrlByNameCB InitCtrlByNameCB) *gin.Engine {
